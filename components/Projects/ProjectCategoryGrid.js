a494a722df7a0d3d0201c733aad4c63eea28315e
Joe <joe@joeebner.com>
2022-03-15 18:30:39 -0700

    fixed import. started working on blog logic

--- components/Projects/ProjectCategoryGrid.js
+++ components/Projects/ProjectCategoryGrid.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import projectCategories from '../../pages/api/projectCategories';
-// what will I use for the URL?
 
 const ProjectCategoryGrid = () => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  /*
+  1. fetch allBlogPosts
+  2. fetch allBlogPostCategories
+  3. create new array of objects called blogCategoryTiles
+    a. for each allBlogPosts.category name, find allBlogPostCategory object name that matches allBlogPosts.category name. 
+    b. add that allBlogPostCategory object to blogCategoryTiles 
+  */
+
+  useEffect(() => {
+    fetch(
+      'https://api.airtable.com/v0/appgzYrTBQ3bcGlF0/blog-posts?',
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_API_KEY}`,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((blogPost) => {
+        setBlogPosts(blogPost.records);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const blogCategoriesDupes = [];
+
+  blogPosts.forEach((post) =>
+    blogCategoriesDupes.push(
+      post.fields.categories.split(', ')
+    )
+  );
+
+  const blogCategories = [
+    ...new Set(blogCategoriesDupes.flat()),
+  ];
+  console.log(blogCategories);
+
   return (
     <div className='project-category-grid-area pb-70'>
       <div className='container'>
@@ -19,11 +57,11 @@ const ProjectCategoryGrid = () => {
               className='col-lg-3 col-sm-6'>
               <div className='onboard-equipment-single'>
                 <div className='onboard-equipment-img'>
-                  <Link href={project.url}>
+                  <Link href='/'>
                     <a>
                       <img
-                        src={project.imgUrl}
-                        alt={project.altText}
+                        src='/images/services/services-1.jpg'
+                        alt='this is alt text'
                       />
                     </a>
                   </Link>
